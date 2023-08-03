9c46df1a486349fdb4b1270ecb08c4924d3deabb
carletonewatkins <carletonewatkins@gmail.com>
2023-08-03 15:34:08 -0600

    next rev

---
+++ pyfib.py
@@ -0,0 +1,15 @@
+# calculate the fibonacci sequence
+
+N = int(input("Number of elements in Fibonacci Series, N, (N>2) : "))
+
+# initialize the list with starting elements: 0, 1
+fibonacciSeries = [0, 1]
+
+if N > 2:
+    for i in range(2, N):
+        # next element in series = sum of it's previous two numbers
+        nextElement = fibonacciSeries[i - 1] + fibonacciSeries[i - 2]
+        # append the element to the series
+        fibonacciSeries.append(nextElement)
+
+print(fibonacciSeries)
